eb354fe15714b669fbd7f0379a1bf8a5fc1e413b
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-06-04 10:34:53 +0700

    demo thu

---
+++ demodoan1/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demodoan1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            var html11 = "rrrrdddd";
+            return Content(html11);
+        }
+        [HttpGet("toan2002",Name ="helllo12")]
+        public ActionResult Index1()
+        {
+
+            var html11 = "tesesas";
+            return Content(html11);
+        }
+    }
+}
