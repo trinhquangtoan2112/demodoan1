fd81ec5e64209e0406125849d0d69a853ed835c3
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-07-02 22:01:07 +0700

    thay doi nho

--- demodoan1/Controllers/LoginController.cs
+++ demodoan1/Controllers/LoginController.cs
@@ -28,7 +28,7 @@ namespace demodoan1.Controllers
     [Route("[controller]")]
     public class LoginController : Controller
     {
-      
+
         public readonly DbDoAnTotNghiepContext _appDbContext;
         private readonly AppSetting _appSetting;
         public LoginController(DbDoAnTotNghiepContext appDbContext, IOptions<AppSetting> appSetting)
@@ -92,7 +92,7 @@ namespace demodoan1.Controllers
             try
             {
                 var checkEmail = _appDbContext.Users.FirstOrDefault(item => item.Email == user.Email);
-                if(checkEmail == null)
+                if (checkEmail == null)
                 {
                     user.MatKhau = PasswordEncryptDecord.EncodePasswordToBase64(user.MatKhau);
                     var user1 = new User
@@ -114,7 +114,7 @@ namespace demodoan1.Controllers
                 {
                     return BadRequest(new { status = StatusCodes.Status400BadRequest, data = "Email da ton tai" });
                 }
-              
+
             }
             catch (Exception ex)
             {
@@ -122,14 +122,14 @@ namespace demodoan1.Controllers
             }
         }
         [HttpPost("Login", Name = "Login")]
-        public async Task<IActionResult> Login([FromBody] UserDto user )
+        public async Task<IActionResult> Login([FromBody] UserDto user)
         {
             try
             {
                 user.MatKhau = PasswordEncryptDecord.EncodePasswordToBase64(user.MatKhau);
 
                 var taiKhoan = await _appDbContext.Users.Include(u => u.MaQuyenNavigation).SingleOrDefaultAsync(u => u.Email == user.Email && u.MatKhau == user.MatKhau);
-               
+
                 if (taiKhoan != null)
                 {
                     var responseData = new
@@ -167,7 +167,7 @@ namespace demodoan1.Controllers
                 return BadRequest(ex.Message);
             }
         }
-        
+
         [HttpPost("changeAuthen", Name = "changeAuthen")]
         public async Task<IActionResult> checkToken(string token)
         {
@@ -190,11 +190,11 @@ namespace demodoan1.Controllers
                 else
                 {
                     string subject = "Xac thuc tai khoan";
-                    string link = "https://localhost:7094/Login/authenAccount?check=";
-                    bool ketQua = await sendEmail(dataUser.Email, subject, link,tokenAuthen);
+                    string link = "http://localhost:3000/authenAccount?check=";
+                    bool ketQua = await sendEmail(dataUser.Email, subject, link, tokenAuthen);
                     if (ketQua)
                     {
-                        return Ok(new {status= StatusCodes.Status200OK, message = "Thanh cong" });
+                        return Ok(new { status = StatusCodes.Status200OK, message = "Thanh cong" });
                     }
                     else
                     {
@@ -208,14 +208,16 @@ namespace demodoan1.Controllers
             }
         }
         [HttpPut("authenAccount", Name = "authenAccount")]
-        public async Task<IActionResult> authenAccount(string check)
+        public async Task<IActionResult> authenAccount(string token)
         {
-            try {
-               
-                Dictionary<string, string> claimsData = TokenClass.DecodeToken(check);
+            try
+            {
+                token = token.Trim();
+                var data = token.Substring(7);
+                Dictionary<string, string> claimsData = TokenClass.DecodeToken(data);
                 string iDNguoiDung = claimsData["IdUserName"];
                 var dataUser = _appDbContext.Users.FirstOrDefault(item => item.MaNguoiDung == Int64.Parse(iDNguoiDung));
-                if (dataUser.TrangThai==true)
+                if (dataUser.TrangThai == true)
                 {
                     return BadRequest(new { Success = 400, data = "Tai khoan da xac thuc" });
                 }
@@ -227,8 +229,8 @@ namespace demodoan1.Controllers
                     return Ok(new { Success = 200, data = "Xac thuc thanh cong" });
                 }
             }
-            
-            catch(Exception ex)
+
+            catch (Exception ex)
             {
                 return BadRequest(new { Success = 400, data = "Co loi xay ra" });
             }
@@ -236,9 +238,9 @@ namespace demodoan1.Controllers
         [HttpGet("forgetPassword", Name = "forgetPassword")]
         public async Task<IActionResult> ForgetPassword(string gmail)
         {
-          
+
             var dataUser = _appDbContext.Users.FirstOrDefault(item => item.Email == gmail);
-            if(dataUser == null)
+            if (dataUser == null)
             {
                 return NotFound(new { Success = StatusCodes.Status404NotFound, data = "Khong tim thay" });
             }
@@ -252,7 +254,7 @@ namespace demodoan1.Controllers
                 else
                 {
                     string subject = "Thay đổi mật khẩu";
-                    string link = "https://localhost:7094/Login/ChangePassword?token=bearer%20";
+                    string link = "http://localhost:3000/ChangePassword?token=bearer%20";
                     bool ketQua = await sendEmail(dataUser.Email, subject, link, tokenAuthen);
                     if (ketQua)
                     {
@@ -265,12 +267,12 @@ namespace demodoan1.Controllers
                     }
 
                 }
-               
+
             }
-           
+
         }
         [HttpPut("ChangePassword", Name = "ChangePassword")]
-        public async Task<IActionResult> ChangePassword(string token,string password)
+        public async Task<IActionResult> ChangePassword(string token, string password)
         {
             try
             {
@@ -293,7 +295,7 @@ namespace demodoan1.Controllers
 
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(new { Success = StatusCodes.Status400BadRequest, data = "Lỗi" });
 
@@ -323,8 +325,8 @@ namespace demodoan1.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
         }
-      
-        public async  static Task<bool> SendMail(string _from, string _to, string _subject, string _body, SmtpClient client)
+
+        public async static Task<bool> SendMail(string _from, string _to, string _subject, string _body, SmtpClient client)
         {
             // Tạo nội dung Email
             MailMessage message = new MailMessage(
@@ -427,12 +429,12 @@ namespace demodoan1.Controllers
         }
         [HttpPut("CapNhapThongtinNguoiDung", Name = "CapNhapThongtinNguoiDung")]
 
-        public async Task<IActionResult> CapNhapThongtinNguoiDung([FromBody] AdduserDto adduser , string token)
+        public async Task<IActionResult> CapNhapThongtinNguoiDung([FromBody] AdduserDto adduser, string token)
         {
             try
             {
-               string tokenData = TokenClass.Decodejwt(token);
-                if(Int64.Parse(tokenData) == adduser.maNguoiDung)
+                string tokenData = TokenClass.Decodejwt(token);
+                if (Int64.Parse(tokenData) == adduser.maNguoiDung)
                 {
                     var dataUser = _appDbContext.Users.FirstOrDefault(item => item.MaNguoiDung == Int64.Parse(tokenData));
                     if (dataUser == null)
@@ -449,7 +451,6 @@ namespace demodoan1.Controllers
                         dataUser.TenNguoiDung = adduser.TenNguoiDung != null ? adduser.TenNguoiDung : dataUser.TenNguoiDung;
                         dataUser.GioiTinh = adduser.GioiTinh != null ? adduser.GioiTinh : dataUser.GioiTinh;
                         dataUser.NgaySinh = adduser.NgaySinh != null ? adduser.NgaySinh : dataUser.NgaySinh;
-
                         _appDbContext.Users.Update(dataUser);
                         _appDbContext.SaveChanges();
                         return Ok(new
@@ -463,14 +464,15 @@ namespace demodoan1.Controllers
                 return Unauthorized(new
                 {
                     status = StatusCodes.Status401Unauthorized,
-                    data ="Khong co quyen thay doi"
+                    data = "Khong co quyen thay doi"
                 });
-              
+
             }
             catch (Exception ex)
             {
-
-
+                return BadRequest(ex);
+            }
+        }
 
         // Thêm tài khoản bởi admin
         [HttpPost("AddUserByAdmin", Name = "AddUserByAdmin")]
@@ -593,27 +595,21 @@ namespace demodoan1.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
-            }
-        }
-    
-
-                return BadRequest( new
+                return BadRequest(new
                 {
                     status = StatusCodes.Status404NotFound,
-                    data ="Lỗi"
+                    data = "Lỗi"
                 });
             }
-           
         }
         [HttpGet("SearchUser", Name = "SearchUser")]
         public async Task<IActionResult> SearchUser(string search)
         {
             try
             {
-         
-                var dataUser = _appDbContext.Users.Where(item => item.TenNguoiDung ==search || item.Email ==search).ToList();
-                if (dataUser.Count ==0)
+
+                var dataUser = _appDbContext.Users.Where(item => item.TenNguoiDung == search || item.Email == search).ToList();
+                if (dataUser.Count == 0)
                 {
                     return NotFound(new
                     {
@@ -623,7 +619,7 @@ namespace demodoan1.Controllers
                 }
                 else
                 {
-                   
+
                     return Ok(new
                     {
                         success = StatusCodes.Status200OK,
@@ -645,7 +641,7 @@ namespace demodoan1.Controllers
 
         }
         [HttpPost("testHeader", Name = "testHeader")]
-      
+
         public async Task<IActionResult> testHeader(string token)
         {
             var role = TokenClass.DecodejwtForRoles(token);
@@ -655,6 +651,52 @@ namespace demodoan1.Controllers
                 Authorization = role
             });
         }
+        [HttpGet("updateInfo", Name = "updateInfo")]
 
+        public async Task<IActionResult> updateInfo(string token)
+        {
+            token = token.Trim();
+            var data = token.Substring(7);
+            Dictionary<string, string> claimsData = TokenClass.DecodeToken(data);
+            string iDNguoiDung = claimsData["IdUserName"];
+            var taiKhoan = _appDbContext.Users.Include(u => u.MaQuyenNavigation).FirstOrDefault(item => item.MaNguoiDung == Int64.Parse(iDNguoiDung));
+            if (taiKhoan != null)
+            {
+                var responseData = new
+                {
+                    MaNguoiDung = taiKhoan.MaNguoiDung,
+                    TenNguoiDung = taiKhoan.TenNguoiDung,
+                    MatKhau = PasswordEncryptDecord.DecodeFrom64(taiKhoan.MatKhau),
+                    Email = taiKhoan.Email,
+                    NgaySinh = taiKhoan.NgaySinh,
+                    GioiTinh = taiKhoan.GioiTinh,
+                    AnhDaiDien = taiKhoan.AnhDaiDien,
+                    TrangThai = taiKhoan.TrangThai,
+                    DaXoa = taiKhoan.DaXoa,
+                    SoDeCu = taiKhoan.SoDeCu,
+                    SoXu = taiKhoan.SoXu,
+                    SoChiaKhoa = taiKhoan.SoChiaKhoa,
+                    Vip = taiKhoan.Vip,
+                    NgayHetHanVip = taiKhoan.NgayHetHanVip,
+                    NgayTao = taiKhoan.Ngaytao,
+                    NgayCapNhap = taiKhoan.NgayCapNhap,
+                    MaQuyen = taiKhoan.MaQuyen
+
+                };
+                return Ok(new
+                {
+                    status = StatusCodes.Status200OK,
+                    data = responseData
+                });
+            }
+            else
+            {
+                return NotFound(new
+                {
+                    status = StatusCodes.Status404NotFound,
+                    data = "Khong tim thay"
+                });
+            }
+        }
     }
 }
