9a650c8fc8bb89d7a1bc48067a711c681b7b35b4
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-07-07 19:59:55 +0700

    thay doi chuong truyen

--- demodoan1/Controllers/ChuongtruyensController.cs
+++ demodoan1/Controllers/ChuongtruyensController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using demodoan1.Models;
 using demodoan1.Models.ChuongtruyenDto;
+using demodoan1.Models.TruyenDto;
+using demodoan1.Helpers;
 
 namespace demodoan1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ChuongtruyensController : ControllerBase
     {
@@ -22,49 +24,134 @@ namespace demodoan1.Controllers
         }
 
         // GET: api/Chuongtruyens
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Chuongtruyen>>> GetChuongtruyens()
+
+        [HttpGet("Danhsachchuong")]
+        public async Task<ActionResult> GetChuongtruyens([FromQuery] int maTruyen,[FromQuery] string? token)
         {
-            return await _context.Chuongtruyens.ToListAsync();
+            string tokenData =null;
+            if (!string.IsNullOrEmpty(token))
+            {
+                 tokenData = TokenClass.Decodejwt(token);
+            }
+          
+            var taiKhoan = await _context.Chuongtruyens.Where(item =>item.MaTruyen ==maTruyen).ToListAsync();
+            if (taiKhoan.Count == 0)
+            {
+                return NotFound(new { status = StatusCodes.Status404NotFound, message = "Không tìm thấy" });
+            }
+           
+            var responseData = taiKhoan.Select(u => new
+            {
+                Machuongtruyen = u.MaChuong,
+                TenChuong = u.TenChuong,
+                TrangThai = u.TrangThai,
+                NoiDung = u.NoiDung,
+                HienThi = u.HienThi,
+                GiaChuong = u.GiaChuong,
+                LuotDoc =u.LuotDoc,
+                Stt =u.Stt,
+                NgayTao = u.Ngaytao,
+                NgayCapNhat = u.NgayCapNhap,
+                Damua = u.GiaChuong > 0
+        ? tokenData!=null?  _context.Giaodiches.Any(g => g.MaChuongTruyen == u.MaChuong && g.MaNguoiDung == Int64.Parse(tokenData)):false
+        : (bool?)true // Return null if GiaChuong is not greater than 0
+            }).ToList();
+
+            return Ok(new
+            {
+                status = StatusCodes.Status200OK,
+                data = responseData
+            });
+           
         }
 
         // GET: api/Chuongtruyens/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Chuongtruyen>> GetChuongtruyen(int id)
+        [HttpGet("GetChiTietChuong")]
+        public async Task<ActionResult<Chuongtruyen>> GetChuongtruyen([FromQuery] int maChuong, [FromQuery] string? token)
         {
-            var chuongtruyen = await _context.Chuongtruyens.FindAsync(id);
-
-            if (chuongtruyen == null)
+            try
             {
-                return NotFound();
-            }
+                var currentChapter = _context.Chuongtruyens.FirstOrDefault(item => item.MaChuong == maChuong &&item.TrangThai !=0);
 
-            return chuongtruyen;
+                if (currentChapter == null)
+                {
+                    return NotFound();
+
+                }
+                else
+                {
+                    var hasNextChapter = await _context.Chuongtruyens
+        .AnyAsync(c => c.MaTruyen == currentChapter.MaTruyen && c.Stt > currentChapter.Stt);
+                    if (currentChapter.GiaChuong == 0)
+                    {
+                        var responseData = new
+                        {
+                            Machuongtruyen = currentChapter.MaChuong,
+                            TenChuong = currentChapter.TenChuong,
+                            TrangThai = currentChapter.TrangThai,
+                            NoiDung = currentChapter.NoiDung,
+                            HienThi = currentChapter.HienThi,
+                            GiaChuong = currentChapter.GiaChuong,
+                            LuotDoc = currentChapter.LuotDoc,
+                            Stt = currentChapter.Stt,
+                            NgayTao = currentChapter.Ngaytao,
+                            NgayCapNhat = currentChapter.NgayCapNhap,
+                            trangTiep = hasNextChapter
+                        };
+                        return Ok(new { StatusCode = StatusCodes.Status200OK, data = responseData });
+                    }
+                    else
+                    {
+                        string tokenData = null;
+                        if (!string.IsNullOrEmpty(token))
+                        {
+                            tokenData = TokenClass.Decodejwt(token);
+                            Boolean daMua = tokenData != null ? _context.Giaodiches.Any(g => g.MaChuongTruyen == maChuong && g.MaNguoiDung == Int64.Parse(tokenData)) : false;
+                            if (!daMua)
+                            {
+                                return Unauthorized();
+                            }
+                            var responseData = new
+                            {
+                                Machuongtruyen = currentChapter.MaChuong,
+                                TenChuong = currentChapter.TenChuong,
+                                TrangThai = currentChapter.TrangThai,
+                                NoiDung = currentChapter.NoiDung,
+                                HienThi = currentChapter.HienThi,
+                                GiaChuong = currentChapter.GiaChuong,
+                                LuotDoc = currentChapter.LuotDoc,
+                                Stt = currentChapter.Stt,
+                                NgayTao = currentChapter.Ngaytao,
+                                NgayCapNhat = currentChapter.NgayCapNhap,
+                                trangTiep = hasNextChapter
+                            };
+                            return Ok(new
+                            {
+                                StatusCodes.Status200OK,
+                                Ddata = responseData
+                            });
+                        }
+                        return Unauthorized();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+            }
+            
+
+           
+
+            
         }
 
         // PUT: api/Chuongtruyens/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutChuongtruyen(int id, ChuongtruyenDto chuongtruyenDto)
+        public async Task<ActionResult> PutChuongtruyen(int id, ChuongtruyenDto chuongtruyenDto)
         {
-            if (id != chuongtruyenDto.MaChuong)
-            {
-                return BadRequest();
-            }
-
-            var chuongtruyen = new Chuongtruyen
-            {
-                MaChuong = chuongtruyenDto.MaChuong,
-                TenChuong = chuongtruyenDto.TenChuong,
-                TrangThai = chuongtruyenDto.TrangThai,
-                NoiDung = chuongtruyenDto.NoiDung,
-                HienThi = chuongtruyenDto.HienThi,
-                GiaChuong = chuongtruyenDto.GiaChuong,
-                LuotDoc = chuongtruyenDto.LuotDoc,
-                MaTruyen = chuongtruyenDto.MaTruyen
-            };
-
-            _context.Entry(chuongtruyen).State = EntityState.Modified;
+          
 
             try
             {
@@ -88,23 +175,42 @@ namespace demodoan1.Controllers
         // POST: api/Chuongtruyens
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Chuongtruyen>> PostChuongtruyen(ChuongtruyenDto chuongtruyenDto)
+        public async Task<ActionResult<Chuongtruyen>> PostChuongtruyen([FromBody]ChuongtruyenDto chuongtruyenDto)
         {
-            var chuongtruyen = new Chuongtruyen
+
+            try
             {
-                TenChuong = chuongtruyenDto.TenChuong,
-                TrangThai = chuongtruyenDto.TrangThai,
-                NoiDung = chuongtruyenDto.NoiDung,
-                HienThi = chuongtruyenDto.HienThi,
-                GiaChuong = chuongtruyenDto.GiaChuong,
-                LuotDoc = chuongtruyenDto.LuotDoc,
-                MaTruyen = chuongtruyenDto.MaTruyen
-            };
+                var maxStt = await _context.Chuongtruyens
+                                              .Where(d => d.MaTruyen == chuongtruyenDto.MaTruyen)
+                                              .MaxAsync(d => (int?)d.Stt) ?? 0;
+                var chuongtruyen = new Chuongtruyen
+                {
+                    TenChuong = chuongtruyenDto.TenChuong,
+                    TrangThai = 1,
+                    NoiDung = chuongtruyenDto.NoiDung,
+                    HienThi = 1,
+                    GiaChuong = chuongtruyenDto.GiaChuong,
+                    LuotDoc = 0,
+                    MaTruyen = chuongtruyenDto.MaTruyen,
+                    Stt = maxStt + 1
+                };
 
-            _context.Chuongtruyens.Add(chuongtruyen);
-            await _context.SaveChangesAsync();
+                _context.Chuongtruyens.Add(chuongtruyen);
+                await _context.SaveChangesAsync();
+                var response = new
+                {
+                    Status = StatusCodes.Status201Created,
+                    Message = "Thêm chương truyện mới",
+                    Data = chuongtruyenDto
+                };
+                return Created("Them truyen thành công", response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { status = StatusCodes.Status400BadRequest, data = chuongtruyenDto });
 
-            return CreatedAtAction("GetChuongtruyen", new { id = chuongtruyen.MaChuong }, chuongtruyen);
+            }
+
         }
 
         // DELETE: api/Chuongtruyens/5
--- demodoan1/Models/ChuongtruyenDto/ChuongtruyenDto.cs
+++ demodoan1/Models/ChuongtruyenDto/ChuongtruyenDto.cs
@@ -2,20 +2,9 @@
 {
     public class ChuongtruyenDto
     {
-        public int MaChuong { get; set; }
-
         public string? TenChuong { get; set; }
-
-        public int? TrangThai { get; set; }
-
         public string? NoiDung { get; set; }
-
-        public int? HienThi { get; set; }
-
         public int? GiaChuong { get; set; }
-
-        public int? LuotDoc { get; set; }
-
         public int MaTruyen { get; set; }
     }
 }
