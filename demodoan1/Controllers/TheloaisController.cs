b81b9de5c9a2bb8f5ac4c351659caf71dbdb715b
DucNguyen2002 <96508058+DucNguyen2002@users.noreply.github.com>
2024-07-11 21:55:49 +0700

    quản lý thế loại

--- demodoan1/Controllers/TheloaisController.cs
+++ demodoan1/Controllers/TheloaisController.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using demodoan1.Models;
 using demodoan1.Models.TheloaiDto;
 using demodoan1.Data;
+using demodoan1.Helpers;
+using NuGet.Common;
+using demodoan1.Models.ButdanhDto;
 
 namespace demodoan1.Controllers
 {
@@ -24,10 +27,31 @@ namespace demodoan1.Controllers
 
         // GET: api/Theloais
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Theloai>>> GetTheloais()
+        public async Task<ActionResult<IEnumerable<GetTheloaiDto>>> GetTheloais(string token)
         {
-            return await _context.Theloais.ToListAsync();
+            var data = token.Trim().Substring(7);
+            var claimsData = TokenClass.DecodeToken(data);
+            var iDNguoiDung = claimsData["IdUserName"];
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.MaNguoiDung == Int64.Parse(iDNguoiDung));
+            if (user == null || user.MaQuyen != 1)
+            {
+                return Unauthorized(new { status = StatusCodes.Status401Unauthorized, message = "Không có quyền" });
+            }
+
+            var theloaiList = await _context.Theloais
+                .Select(tl => new GetTheloaiDto
+                {
+                    MaTheLoai = tl.MaTheLoai,
+                    TenTheLoai = tl.TenTheLoai,
+                    MoTa = tl.MoTa,
+                    Soluongtruyen = _context.Truyens.Count(tr => tr.MaTheLoai == tl.MaTheLoai).ToString()
+                })
+                .ToListAsync();
+
+            return Ok(new { status = StatusCodes.Status200OK, data = theloaiList });
         }
+
 
         // GET: api/Theloais/5
         [HttpGet("{id}")]
@@ -44,90 +68,111 @@ namespace demodoan1.Controllers
         }
 
         // PUT: api/Theloais/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTheloai(int id, TheloaiDto theloaiDto)
+        [HttpPut]
+        public async Task<IActionResult> PutTheloai(TheloaiDto theloaiDto, [FromQuery] string token)
         {
             try
             {
-                var theloai = await _context.Theloais.FindAsync(id);
-
-                if (theloai == null)
+                token = token.Trim();
+                var data = token.Substring(7);
+                Dictionary<string, string> claimsData = TokenClass.DecodeToken(data);
+                string iDNguoiDung = claimsData["IdUserName"];
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.MaNguoiDung == Int64.Parse(iDNguoiDung));
+                if (user == null || user.MaQuyen != 1)
                 {
-                    return NotFound();
+                    return Unauthorized(new { status = StatusCodes.Status401Unauthorized, message = "Không có quyền" });
                 }
 
+                var theloai = await _context.Theloais.FindAsync(theloaiDto.MaTheLoai);
+                if (theloai == null)
+                {
+                    return NotFound(new { status = StatusCodes.Status404NotFound, message = "Thể loại không tồn tại" });
+                }
+
+                // Cập nhật các thuộc tính của thể loại bằng dữ liệu từ theloaiDto
                 theloai.TenTheLoai = theloaiDto.TenTheLoai;
                 theloai.MoTa = theloaiDto.MoTa;
 
                 _context.Entry(theloai).State = EntityState.Modified;
-
                 await _context.SaveChangesAsync();
 
-                return NoContent();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TheloaiExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return Ok(new { status = StatusCodes.Status200OK, data = theloaiDto });
             }
             catch (Exception ex)
             {
-              
                 return StatusCode(StatusCodes.Status500InternalServerError, new { Message = "Lỗi khi cập nhật thể loại.", Exception = ex.Message });
             }
         }
 
-    
+
         [HttpPost]
-        public async Task<ActionResult> PostTheloai([FromBody] TheloaiDto theloaiDto)
+        public async Task<ActionResult<Theloai>> PostButdanh([FromBody] TheloaiDto theloaiDto, [FromQuery] string token)
         {
-            if (theloaiDto == null)
-            {
-                return BadRequest(new { Success = 400, Message = "Invalid data." });
-            }
-
-            var theloai = new Theloai
-            {
-                TenTheLoai = theloaiDto.TenTheLoai,
-                MoTa = theloaiDto.MoTa,
-                Trangthai = 0,
-            };
-
             try
             {
-                _context.Theloais.Add(theloai);
+                Boolean daCo = _context.Theloais.Any(item => item.TenTheLoai.Equals(theloaiDto.TenTheLoai));
+                if (daCo)
+                    return BadRequest(new { status = StatusCodes.Status400BadRequest, message = "Đã tồn tại" });
+                token = token.Trim();
+                var data = token.Substring(7);
+                Dictionary<string, string> claimsData = TokenClass.DecodeToken(data);
+                string iDNguoiDung = claimsData["IdUserName"];
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.MaNguoiDung == Int64.Parse(iDNguoiDung));
+                if (user == null || user.MaQuyen != 1)
+                {
+                    return Unauthorized(new { status = StatusCodes.Status401Unauthorized, message = "Không có quyền" });
+                }
+                var tl = new Theloai
+                {
+                    TenTheLoai = theloaiDto.TenTheLoai,
+                    MoTa = theloaiDto.MoTa,
+                };
+
+                _context.Theloais.Add(tl);
                 await _context.SaveChangesAsync();
-                return Ok(new { Success = 200, Data = theloaiDto });
+
+                return Ok(new { status = StatusCodes.Status201Created, data = theloaiDto });
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { Success = 500, Message = "An error occurred while saving the data.", Error = ex.Message });
+                return BadRequest(new { status = StatusCodes.Status400BadRequest, message = "lỗi" });
             }
+
         }
 
 
-      
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTheloai(int id)
+        public async Task<IActionResult> DeleteTheloai(int id, [FromQuery] string token)
         {
-            var theloai = await _context.Theloais.FindAsync(id);
-            if (theloai == null)
+            try
             {
-                return NotFound();
+                token = token.Trim();
+                var data = token.Substring(7);
+                Dictionary<string, string> claimsData = TokenClass.DecodeToken(data);
+                string iDNguoiDung = claimsData["IdUserName"];
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.MaNguoiDung == Int64.Parse(iDNguoiDung));
+                if (user == null || user.MaQuyen != 1)
+                {
+                    return Unauthorized(new { status = StatusCodes.Status401Unauthorized, message = "Không có quyền" });
+                }
+
+                var theloai = await _context.Theloais.FindAsync(id);
+                if (theloai == null)
+                {
+                    return NotFound(new { status = StatusCodes.Status404NotFound, message = "Thể loại không tồn tại" });
+                }
+
+                _context.Theloais.Remove(theloai);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { status = StatusCodes.Status204NoContent, message = "Xóa thể loại thành công" });
             }
-
-            _context.Theloais.Remove(theloai);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Message = "Lỗi khi xóa thể loại.", Exception = ex.Message });
+            }
         }
+
 
         private bool TheloaiExists(int id)
         {
--- demodoan1/Models/TheloaiDto/TheloaiDto.cs
+++ demodoan1/Models/TheloaiDto/TheloaiDto.cs
@@ -2,8 +2,22 @@
 {
     public class TheloaiDto
     {
+        public int MaTheLoai { get; set; }
         public string? TenTheLoai { get; set; }
         public string? MoTa { get; set; }
     }
+
+    public class GetTheloaiDto
+    {
+        public int MaTheLoai { get; set; }
+
+        public string? TenTheLoai { get; set; }
+
+        public string? MoTa { get; set; }
+        public string? Soluongtruyen { get; set; }
+
+    }
+
+
 
 }
