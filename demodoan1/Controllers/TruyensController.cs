a8d67071657c22c685bdbfda87c480d8c610ea04
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-07-25 19:42:03 +0700

    Quan ly truyen chuong truyen

--- demodoan1/Controllers/TruyensController.cs
+++ demodoan1/Controllers/TruyensController.cs
@@ -229,7 +229,7 @@ namespace demodoan1.Controllers
                 Page = page,
                 PageSize = pageSize,
                 Data = truyen.Chuongtruyens
-                    .Where(ch => ch.TrangThai != 0 && ch.HienThi != 0)
+                    .Where(ch => ch.TrangThai != 0 && ch.HienThi != 0 &&ch.TrangThai !=4)
                     .OrderBy(ch => ch.Stt)
                     .Skip(skip)
                     .Take(take)
@@ -293,7 +293,7 @@ namespace demodoan1.Controllers
                 Page = page,
                 PageSize = pageSize,
                 Data = truyen.Chuongtruyens
-                    .Where(ch => ch.TrangThai != 3 && ch.HienThi != 0)
+                    .Where(ch => ch.TrangThai != 4 && ch.HienThi != 0 &&ch.TrangThai !=4)
                     .OrderBy(ch => ch.Stt)
                     .Skip(skip)
                     .Take(take)
